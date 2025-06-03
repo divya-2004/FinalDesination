53c3dd82df5e47368fec361d78b68431e86137ca
Divya <112820749+divya-2004@users.noreply.github.com>
2025-06-03 20:57:18 +0530

    Update CustomerService.java

--- Final-Destination-main/src/main/service/CustomerService.java
+++ Final-Destination-main/src/main/service/CustomerService.java
@@ -1,7 +1,6 @@
 package com.examly.service;
-
 import com.examly.entity.Customer;
 import com.examly.exception.EmailAlreadyRegisteredException;
 public interface CustomerService {
     boolean createCustomer(Customer customer) throws EmailAlreadyRegisteredException;
-}
+} 
